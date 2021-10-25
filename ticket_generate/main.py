bee54444449e5120575c2636de3f3f4c591f8fe7
Pranav <prajpran@gmail.com>
2021-10-26 01:30:14 +0530

    added the new tmeplate and modified some settings to make it look good

--- ticket_generate/main.py
+++ ticket_generate/main.py
@@ -16,28 +16,35 @@ ticketsCollection.delete_many({})
 
 GOTENBERG = "http://localhost:3000/forms/chromium/convert/html"
 
-tickets = [{'firstName': 'Noah', 'lastName': 'Reilly', 'amount': 300},
-           {'firstName': 'Lori', 'lastName': 'Stone', 'amount': 500},
-           {'firstName': 'Brian', 'lastName': 'Wilkerson', 'amount': 500},
-           {'firstName': 'Jose', 'lastName': 'Jackson', 'amount': 500},
-           {'firstName': 'Melanie', 'lastName': 'White', 'amount': 300},
-           {'firstName': 'Mary', 'lastName': 'White', 'amount': 500},
-           {'firstName': 'Angela', 'lastName': 'Cox', 'amount': 500},
-           {'firstName': 'Scott', 'lastName': 'Scott', 'amount': 300},
-           {'firstName': 'Kathryn', 'lastName': 'Romero', 'amount': 500},
-           {'firstName': 'Richard', 'lastName': 'Benitez', 'amount': 500}]
+tickets = [{'firstName': 'Noah', 'lastName': 'Reilly', 'amount': 300,"branch":"CSE"},
+           {'firstName': 'Lori', 'lastName': 'Stone', 'amount': 500,"branch":"CSE"},
+           {'firstName': 'Brian', 'lastName': 'Wilkerson', 'amount': 500,"branch":"CSE"},
+           {'firstName': 'Jose', 'lastName': 'Jackson', 'amount': 500,"branch":"CSE"},
+           {'firstName': 'Melanie', 'lastName': 'White', 'amount': 300,"branch":"CSE"},
+           {'firstName': 'Mary', 'lastName': 'White', 'amount': 500,"branch":"CSE"},
+           {'firstName': 'Angela', 'lastName': 'Cox', 'amount': 500,"branch":"CSE"},
+           {'firstName': 'Scott', 'lastName': 'Scott', 'amount': 300,"branch":"CSE"},
+           {'firstName': 'Kathryn', 'lastName': 'Romero', 'amount': 500,"branch":"CSE"},
+           {'firstName': 'Richard', 'lastName': 'Benitez', 'amount': 500,"branch":"CSE"}
+]
 
 paperData = {
-	'paperWidth': 8.5,
-	'paperHeight': 11,
-	"marginTop": 0.2,
-	"marginBottom": 0.2,
-	"marginLeft": 0.2,
-	"marginRight": 0.2
+	'paperWidth': 3.8,
+	'paperHeight': 6.19,
+	"marginTop": 0.05,
+	"marginBottom": 0.05,
+	"marginLeft": 0.05,
+	"marginRight": 0.05
 }
 
 with open("ticket.html","r") as file:
 	htmlFile = file.read()
+
+with open("oysterbay.jpeg","rb") as file:
+	oysterBayImage = file.read()
+
+with open("style.css","r") as file:
+	cssStyles = file.read()
 
 template = Template(htmlFile)
 
@@ -63,7 +70,10 @@ for userData in tickets:
 	output = template.render(userData)
 	outputFile = BytesIO(bytes(output,encoding='utf-8'))
 
-	response = post(GOTENBERG, data = paperData, files=[('index.html',outputFile),("qrCode.png",qrCodePng)])
+	response = post(GOTENBERG, data = paperData, files=[('index.html',outputFile),
+														('oysterbay.jpeg',oysterBayImage),
+														('style.css',cssStyles),
+														("qrCode.png",qrCodePng)])
 	pdfFile = BytesIO(response.content)
 
 	with open(f"{userData['firstName']}_{userData['lastName']}.pdf","wb") as file:
